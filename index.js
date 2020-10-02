0aea188c11ccfce2c8dc190309f26d5c428e4774
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-10-02 15:00:05 -0700

    fixed the write function by adding return

--- index.js
+++ index.js
@@ -72,11 +72,12 @@ async function start(){
             message: "Go the extra mile and write tests for your application. Then provide examples on how to run them.`"
         },   
         
-    ]).then(function(data){
-        console.log("data", data)
+    ]).then(function(answer){
+        console.log("data", answer)
         // function to create the readme from the template
 
         const readmeTemp = readmeTemplate(answer)
+        console.log("readmeTemp", readmeTemp)
         //this takes 3 parameters; file name, content, callback function err
         fs.writeFile("generatedReadme.md", readmeTemp, function(err){
             if(err){
@@ -85,20 +86,19 @@ async function start(){
                 console.log("Wright complete!")
             }
         })
-    })
-
-
-
-    // function to generate readme 
-    function readmeTemplate(answer) {
-    `# ${answer.projectTitle}                                           #### License: ![GitHub License](https://img.shields.io/github/license/${answer.gitUser}/${answer.repoName})
-                                                                                        [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
-                                                                                        
-        ## Description
     
+        // function to generate readme 
+        function readmeTemplate(answer) {
+        
+        return `# ${answer.projectTitle}   
+        #### License: ![GitHub License](https://img.shields.io/github/license/${answer.gitUser}/${answer.repoName})
+        [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
+                                                                                            
+        ## Description
+        
         ${answer.description}
 
-        
+            
         ## Table of Contents
 
         - [Installation](#Installation)
@@ -108,35 +108,35 @@ async function start(){
         - [Badges](#Badges)
         - [Contributing](#Contributing)
         - [Tests](#Tests)
-        
-        
+            
+            
         ## Installation
         Install Contributor Covenant by executing the following commands in command terminal: npm install -g covgen; covgen "<your_email_address>"
         ${answer.installation}
-    
+        
 
         ## Usage 
-    
-        ${answer.usage}
         
+        ${answer.usage}
+            
         ${answer.screenshot}
 
-        
+            
         ## Credits
 
-            ### Collaborators
-        
-            ${answer.collaborator} ${answer.collaboratorLink}
+        ### Collaborators
+            
+        ${answer.collaborator} ${answer.collaboratorLink}
 
-            ### Third Party Assets
-        
-            ${answer.thirdPartyAsset}
+        ### Third Party Assets
+            
+        ${answer.thirdPartyAsset}
 
-            ### Tutorials 
-        
-            ${answer.tutorialCredit}
+        ### Tutorials 
+            
+        ${answer.tutorialCredit}
 
-        
+            
         ## License
 
         This project is using the standard ${answer.license}. See [LICENSE](.LICENSE) for Terms and Conditions.
@@ -147,26 +147,18 @@ async function start(){
         ![GitHub language count](https://img.shields.io/github/languages/count/${answer.gitUser}/${answer.repoName})
         ![GitHub repo size](https://img.shields.io/github/repo-size/${answer.gitUser}/${answer.reponame})
 
-        
+            
         ## Contributing
 
         [Contributor Covenant](.CODE_OF_CONDUCT.md)
-        
-        
+            
+            
         ## Tests
 
-        ${answers.test}`
+        ${answer.test}`
 
-    }
-}
-start()
+        };
+    });
 
-// function to create the readme from the template
-
-// function to initialize program
-//function init() {
-
-//}
-
-// function call to initialize program
-//init();
+};
+start();
---
+++ test.md
@@ -0,0 +1,66 @@
+# ${answer.projectTitle}   
+#### License: ![GitHub License](https://img.shields.io/github/license/${answer.gitUser}/${answer.repoName})
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
+                                                                                            
+## Description
+        
+${answer.description}
+
+            
+## Table of Contents
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Credits](#credits)
+- [license](#license)
+- [Badges](#Badges)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+            
+            
+ ## Installation
+Install Contributor Covenant by executing the following commands in command terminal: npm install -covgen; covgen "<your_email_address>"
+${answer.installation}
+        
+
+## Usage 
+        
+${answer.usage}
+            
+${answer.screenshot}
+
+            
+## Credits
+
+### Collaborators
+            
+${answer.collaborator}
+${answer.collaboratorLink}
+
+### Third Party Assets
+            
+${answer.thirdPartyAsset}
+
+### Tutorials 
+            
+${answer.tutorialCredit}
+
+            
+## License
+
+This project is using the standard ${answer.license}. See [LICENSE](.LICENSE) for Terms anConditions.
+
+
+## Badges
+
+![GitHub language count](https://img.shields.io/github/languages/count/${answer.gitUser}/${answerepoName})
+![GitHub repo size](https://img.shields.io/github/repo-size/${answer.gitUser}/${answer.reponame})
+
+            
+## Contributing
+
+[Contributor Covenant](.CODE_OF_CONDUCT.md)
+            
+            
+## Tests
+
+${answer.test}
